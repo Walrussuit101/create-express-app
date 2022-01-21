02deb754537af6278f46ce8721f1d5ba0d785fbe
Walrussuit101 <tjefferson@ycp.edu>
2022-01-21 13:56:28 -0500

    verify project name is valid

--- src/handlers/arguments.handler.ts
+++ src/handlers/arguments.handler.ts
@@ -9,8 +9,15 @@ import { arguments } from '../interfaces';
 export const getArguments = (): arguments => {
 	let args = process.argv.splice(2);
 
+	// check all args are present
 	if (args.length !== 3) {
 		throw new Error("E001 - Required arguments not provided");
+	}
+
+	// only allow project name to have letters, numbers, -, and _
+	const regex = new RegExp("^[A-Za-z0-9_-]*$");
+	if (!regex.test(args[1])) { 
+		throw new Error("E002 - Invalid project name provided: \""+args[1]+"\"");
 	}
 
 	return {
--- src/handlers/error.handler.ts
+++ src/handlers/error.handler.ts
@@ -1,3 +1,10 @@
+/**
+ * Get the error's output string based on the error code.
+ * If error code not recognized, returns emptpy string.
+ *
+ * @param errorMessage Error message string to parse
+ * @returns string
+ */
 export const getErrorString = (errorMessage: string): string => {
 	const errorCode = errorMessage.substring(0, 4);
 
@@ -8,6 +15,12 @@ export const getErrorString = (errorMessage: string): string => {
 				"\t1st: Project Directory\n" +
 				"\t2nd: Project Name\n" +
 				"\t3rd: Template\n\n"+
+				"ex: npm start directory/for/project my-project skeleton\n"
+			);
+		case "E002":
+			return(
+				"\nPlease provide a project name containing only the follow characters:\n\n"+
+				"\t a-z, A-Z, 0-9, -, and _\n\n"+
 				"ex: npm start directory/for/project my-project skeleton\n"
 			);
 		default:
