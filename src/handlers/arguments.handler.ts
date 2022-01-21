8603daacb6ee33dc03ffd4a69e6eea109fa1ac43
Walrussuit101 <tjefferson@ycp.edu>
2022-01-21 14:09:20 -0500

    clean doc for arg handler

--- src/handlers/arguments.handler.ts
+++ src/handlers/arguments.handler.ts
@@ -4,7 +4,7 @@ import { arguments } from '../interfaces';
  * Get and clean arguments from user
  *
  * @returns arguments
- * @throws E001 when arguments not provided
+ * @throws E001, E002
  */
 export const getArguments = (): arguments => {
 	let args = process.argv.splice(2);
@@ -17,10 +17,12 @@ export const getArguments = (): arguments => {
 	// only allow project name to have letters, numbers, -, and _
 	const regex = new RegExp("^[A-Za-z0-9_-]*$");
 	const projectName = args[0];
+
 	if (!regex.test(projectName)) { 
 		throw new Error("E002 - Invalid project name provided: \""+projectName+"\"");
 	}
 
+	// returned cleaned parsed args
 	return {
 		projectName: args[0].toLowerCase(),
 		template: args[1].toLowerCase()
