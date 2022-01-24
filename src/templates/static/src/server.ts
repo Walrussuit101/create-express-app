ec2ce05f25ef514b8f3422f2659f025bace27337
Walrussuit101 <tjefferson@ycp.edu>
2022-01-24 10:29:23 -0500

    forgot server file

---
+++ src/templates/static/src/server.ts
@@ -0,0 +1,24 @@
+import express, { Response } from 'express';
+import path from 'path';
+
+const server = express();
+const port = process.env.PORT || 8080;
+
+// return the compiled client js file
+server.get("/bundle.js", (_, res: Response) => {
+	res.sendFile(path.join(__dirname, "client/dist/bundle.js"));
+});
+
+// return the stylesheet
+server.get("/index.css", (_, res: Response) => {
+	res.sendFile(path.join(__dirname, "./client/index.css"));
+});
+
+// return the html file
+server.get("/", (_, res: Response) => {
+	res.sendFile(path.join(__dirname, "./client/index.html"));
+});
+
+server.listen(port, () => {
+	console.log(`Listening @ http://localhost:${port}`);
+});
