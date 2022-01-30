f1dee9d15fdf997b7a4c87afcb1e408b11a980e9
cbrandt6 <cbrandt6@ycp.edu>
2022-01-30 10:41:22 -0500

    Comments

--- src/handlers/directory.handler.ts
+++ src/handlers/directory.handler.ts
@@ -58,7 +58,8 @@ export const deleteProjectDirectory = (projectDir: string): void => {
 }
 
 /**
- * Initialize a git repository in the project directory
+ * Initialize a git repository in the project directory.
+ * This also creates the initial commit
  * 
  * @param projectDir Directory of Project
  * @returns void
@@ -79,45 +80,6 @@ export const initGitRepo = (projectDir: string): void => {
 			}
 			console.log(stdout);
 		})
-		// exec(`git -C ${projectDir} init `, (error, stdout, stderr) => {
-		// 	if (error) {
-		// 		console.log(`error: ${error.message}`);
-		// 		return;
-		// 	}
-		// 	if (stderr) {
-		// 		console.log(`stderr: ${stderr}`);
-		// 		return;
-		// 	}
-		// 	console.log(stdout);
-
-		// }); 
-
-		// // Add project files
-		// exec(`git -C ${projectDir}/ add .`, (error, stdout, stderr) => {
-		// 	if(error){
-		// 		console.log(error.message);
-		// 		return;
-		// 	}
-		// 	if(stderr){
-		// 		console.log(stderr);
-		// 		return;
-		// 	}
-		// 	console.log(stdout);
-		// });
-
-		// // Create initial commit
-		// exec(`git -C ${projectDir}/ commit -m "Initial Commit"`, (error, stdout, stderr) => {
-		// 	if(error){
-		// 		console.log(error.message)
-		// 		return;
-		// 	}
-		// 	if(stderr){
-		// 		console.log(stderr);
-		// 		return;
-		// 	}
-		// 	console.log(stdout);
-		// });
-
 	} catch (error) {
 		console.log(error)
 	}
