172aed8ea99eef5c74d6810c7a64951a8cc123c5
Walrussuit101 <tjefferson@ycp.edu>
2022-02-28 09:50:45 -0500

    add string type / change var names

--- tests/customError.test.ts
+++ tests/customError.test.ts
@@ -32,19 +32,19 @@ describe("log()", () => {
 
     it("calls console.error() 1st with red color, code, and message", () => {
         TestingError.log();
-        const firstErrorCallArgs = consoleErrorMock.mock.calls[0][0];
+        const firstErrorCallArg: string = consoleErrorMock.mock.calls[0][0];
 
-        expect(firstErrorCallArgs).toContain("\x1b[31m");
-        expect(firstErrorCallArgs).toContain(testCode);
-        expect(firstErrorCallArgs).toContain(testMessage);
+        expect(firstErrorCallArg).toContain("\x1b[31m");
+        expect(firstErrorCallArg).toContain(testCode);
+        expect(firstErrorCallArg).toContain(testMessage);
     });
 
     it("calls console.error() 2nd with reset color and detail", () => {
         TestingError.log();
-        const secondErrorCallArgs = consoleErrorMock.mock.calls[1][0];
+        const secondErrorCallArg: string = consoleErrorMock.mock.calls[1][0];
 
-        expect(secondErrorCallArgs).toContain("\x1b[0m");
-        expect(secondErrorCallArgs).toContain(
+        expect(secondErrorCallArg).toContain("\x1b[0m");
+        expect(secondErrorCallArg).toContain(
             "Please provide the minimum arguments:"
         );
     });
