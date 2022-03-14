d2c6c02c385ad8748987df1bf78a7f9c5ca910e5
Walrussuit101 <tjefferson@ycp.edu>
2022-03-14 12:13:32 -0400

    update help string with rest-api changes

--- src/help.ts
+++ src/help.ts
@@ -15,14 +15,14 @@ Template descriptions:
   static:
     This will create a template with a single html, css, and typescript file. Using webpack the typescript file, and any
     required dependencies, will be compiled to a vanilla JS bundle, which the html file will include using a <script/> tag.
-    All file retrieval is ran through an express web server, which only serves the html file, css file, and compiled JS bundle.    
+    All file retrieval is ran through an express web server, which only serves the html file, css file, and compiled JS bundle.
 
   rest-api:
-    This will create a bare-bones REST api server. The server has the following valid routes: GET /, GET /users, and GET /user/:id.
-    The project will have dedicated controller, service, and route directories. Specifically, there is an example user controller,
-    service, and route file. The user service operates on an in-memory array of users to serve in JSON. To add database functionality
-    add queries / ORM calls in the service that returns data for the controller to respond with. The template also defaults to allow
-    requests from localhost:3000 via CORS, this can be changed in the src/index.ts file. 
+    This will create a bare-bones REST api server. The server has the following valid routes: GET /, GET /users, GET /users/:id,
+    PUT /users, and DELETE /users/:id. The project will have dedicated controller, service, and route directories. Specifically,
+    there is an example user controller, service, and route file. The user service operates on an in-memory array of users to serve in JSON.
+    To add database functionality add queries / ORM calls in the service that returns data for the controller to respond with. The template
+    also defaults to allow requests from localhost:3000 via CORS, this can be changed in the src/index.ts file.
 `;
 
 export default helpString;
