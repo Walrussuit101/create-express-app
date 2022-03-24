cd3cd5d96e51318541d520b5b1e12326b1ff4f55
Walrussuit101 <tjefferson@ycp.edu>
2022-03-23 22:51:06 -0400

    tests for directoryHandler.createProjectDirectory()

--- tests/directoryHandler.test.ts
+++ tests/directoryHandler.test.ts
@@ -1,5 +1,8 @@
 import { directoryHandler } from "../src/handlers";
+import fs from "fs-extra";
 import path from "path";
+
+import { CustomError } from "../src/models";
 
 describe("getProjectDirectory()", () => {
     it("returns the cwd when given .", () => {
@@ -16,5 +19,45 @@ describe("getProjectDirectory()", () => {
         const expectedDir = path.join(process.cwd(), projectName);
 
         expect(projectDir).toEqual(expectedDir);
+    });
+});
+
+describe("createProjectDirectory()", () => {
+    let mkdirSyncMock: jest.SpyInstance;
+
+    // setup mocks
+    beforeAll(() => {
+        mkdirSyncMock = jest.spyOn(fs, "mkdirSync").mockImplementation();
+    });
+
+    // restore mock implementations
+    afterAll(() => {
+        jest.restoreAllMocks();
+    });
+
+    // clear mock usage data after each test
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it("creates a directory with the given project directory", () => {
+        const testProjectDir = path.join(process.cwd(), "fake-dir");
+
+        directoryHandler.createProjectDirectory(testProjectDir);
+
+        expect(mkdirSyncMock).toHaveBeenCalledWith(testProjectDir);
+    });
+
+    it("throws E002 when directory already exists", () => {
+        const duplicateDir = path.join(__dirname);
+
+        try {
+            directoryHandler.createProjectDirectory(duplicateDir);
+        } catch (e) {
+            expect(e).toBeInstanceOf(CustomError);
+
+            // use as below since we test for CustomError instance above
+            expect((e as CustomError).code).toStrictEqual("E002");
+        }
     });
 });
